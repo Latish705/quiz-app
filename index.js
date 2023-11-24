55e6ba5a1d0557ac46129c999258372c20eb7a41
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2023-11-24 11:09:38 +0530

    finished quiz app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let apiUrl = "https://opentdb.com/api.php?amount=10&category=28&type=multiple";
+let apiUrl = "https://opentdb.com/api.php?amount=10&type=multiple";
 
+const quizContainer = document.querySelector(".main-container");
 const question = document.querySelector(".question");
 const answer1 = document.querySelector(".answer1");
 const answer2 = document.querySelector(".answer2");
@@ -7,8 +8,10 @@ const answer3 = document.querySelector(".answer3");
 const answer4 = document.querySelector(".answer4");
 
 const nextButton = document.querySelector(".next-button");
+let selectedAnswerUser = [];
 
 let currentQuestionIndex = 0;
+let noOfCorrectAnswers = 0;
 
 //first we need to get quesiton and answer
 
@@ -19,21 +22,21 @@ async function getQuestionAnswer() {
     .then((response) => response.json())
     .then((data) => {
       questionData = data.results;
-      console.log(questionData);
       FillDetails(0);
     });
 }
 
 getQuestionAnswer();
+let alreadyClicked = false;
 
 function FillDetails(currentQuestionIndex) {
-  let alreadyClicked = false;
+  alreadyClicked = false;
   question.innerText = questionData[currentQuestionIndex].question;
   let answersArray = [
     ...questionData[currentQuestionIndex].incorrect_answers,
     questionData[currentQuestionIndex].correct_answer,
   ];
-  console.log(answersArray);
+
   shuffleArray(answersArray);
 
   answer1.innerText = answersArray[0];
@@ -48,23 +51,37 @@ function FillDetails(currentQuestionIndex) {
     answer4.addEventListener("click", () => handleAnswerSelection(answer4));
 
     // Function to handle user's answer selection
+    // ...
+
     function handleAnswerSelection(selectedAnswer) {
       // Check if the selected answer is correct
+      if (alreadyClicked) {
+        return; // Do nothing if the user has already clicked
+      }
+      alreadyClicked = true;
       const correctAnswer = questionData[currentQuestionIndex].correct_answer;
+      // console.log("Selected answer:", selectedAnswer.innerText);
+      // console.log("Correct answer:", correctAnswer);
 
       if (selectedAnswer.innerText === correctAnswer) {
         // User selected the correct answer
         selectedAnswer.classList.add("correct-answer");
+        noOfCorrectAnswers++;
+        selectedAnswerUser.push(selectedAnswer.innerText);
+
         // Add your logic for correct answer (e.g., scoring, feedback, etc.)
       } else {
         // User selected the wrong answer
         selectedAnswer.classList.add("wrong-answer");
+        selectedAnswerUser.push(selectedAnswer.innerText);
+
         for (let i = 1; i <= 4; i++) {
           const answerElement = document.querySelector(`.answer${i}`);
           if (answerElement.innerText === correctAnswer) {
             answerElement.classList.add("correct-answer");
           }
         }
+
         // Add your logic for wrong answer (e.g., feedback, etc.)
       }
 
@@ -78,11 +95,6 @@ function FillDetails(currentQuestionIndex) {
 
       // Proceed to the next question
       currentQuestionIndex++;
-      // if (currentQuestionIndex < 10) {
-      //   FillDetails(currentQuestionIndex);
-      // } else {
-      //   console.log("Reached the end of questions");
-      // }
     }
   }
 }
@@ -97,18 +109,73 @@ function shuffleArray(array) {
 //next button functionality
 
 nextButton.addEventListener("click", () => {
-  answer1.classList.remove("correct-answer", "wrong-answer");
-  answer2.classList.remove("correct-answer", "wrong-answer");
-  answer3.classList.remove("correct-answer", "wrong-answer");
-  answer4.classList.remove("correct-answer", "wrong-answer");
-  currentQuestionIndex++;
-  answer1.disabled = false;
-  answer2.disabled = false;
-  answer3.disabled = false;
-  answer4.disabled = false;
-  if (currentQuestionIndex < 10) {
-    FillDetails(currentQuestionIndex);
+  if (!alreadyClicked) {
+    alert("Please Select one answer");
   } else {
-    console.log("hi");
+    answer1.classList.remove("correct-answer", "wrong-answer");
+    answer2.classList.remove("correct-answer", "wrong-answer");
+    answer3.classList.remove("correct-answer", "wrong-answer");
+    answer4.classList.remove("correct-answer", "wrong-answer");
+    currentQuestionIndex++;
+    answer1.disabled = false;
+    answer2.disabled = false;
+    answer3.disabled = false;
+    answer4.disabled = false;
+    if (currentQuestionIndex < 9) {
+      FillDetails(currentQuestionIndex);
+    } else if (currentQuestionIndex == 9) {
+      FillDetails(currentQuestionIndex);
+    } else {
+      quizContainer.removeChild(document.querySelector(".question-div"));
+      quizContainer.removeChild(document.querySelector(".answer-div"));
+      quizContainer.removeChild(nextButton);
+      const showResultsButton = document.createElement("button");
+      showResultsButton.textContent = "Show Your Result";
+      showResultsButton.classList.add("next-button");
+
+      quizContainer.appendChild(showResultsButton);
+
+      showResultsButton.addEventListener("click", () => {
+        quizContainer.removeChild(showResultsButton);
+        quizContainer.style.height = "600px";
+        quizContainer.innerHTML = `
+        <div id="myProgress">
+          <div id="myBar"></div>
+        </div>
+        `;
+        const divele = document.createElement("div");
+        divele.id = "progress-bar";
+        divele.style.width = "100%";
+        const mybardiv = document.createElement("div");
+        mybardiv.id = "myBar";
+
+        mybardiv.style.width = `${noOfCorrectAnswers * 10}%`;
+        mybardiv.style.height = "30px";
+        mybardiv.style.backgroundColor = "#071CA2";
+        mybardiv.style.textAlign = "center";
+        mybardiv.style.lineHeight = "30px"; /* To center it vertically */
+        mybardiv.style.color = "white";
+        divele.appendChild(mybardiv);
+        quizContainer.appendChild(divele);
+
+        const resultContainer = document.createElement("ul");
+        resultContainer.classList.add("list", "answer-div");
+        quizContainer.appendChild(resultContainer);
+        console.log(selectedAnswerUser);
+
+        for (let i = 0; i < 10; i++) {
+          let listItem = document.createElement("li");
+          if (selectedAnswerUser[i] === questionData[i].correct_answer) {
+            listItem.innerHTML = `<span id="correct">Correct :</span>    ${questionData[i].correct_answer}`;
+          } else {
+            listItem.innerHTML = `
+              <span id="correct">Correct :</span> ${questionData[i].correct_answer}                
+              <span id="incorrect">         Your Answer :</span>     ${selectedAnswerUser[i]}
+            `;
+          }
+          resultContainer.appendChild(listItem);
+        }
+      });
+    }
   }
 });
